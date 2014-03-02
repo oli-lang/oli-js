50757ed8ab5f15b60c548fa55dfcec8c358a174f
Tomas Aparicio <tomas@aparicio.me>
2014-03-02 13:46:44 +0100

    chore: update buildignore file

--- oli.js
+++ oli.js
@@ -1,4 +1,4 @@
-/*! oli.js - v0.1.0 - MIT License - https://github.com/oli-lang/oli-js | Generated 2014-03-02 12:33 */
+/*! oli.js - v0.1.0 - MIT License - https://github.com/oli-lang/oli-js | Generated 2014-03-02 01:45 */
 !function(e) {
   if ("object" == typeof exports) module.exports = e(); else if ("function" == typeof define && define.amd) define(e); else {
     var f;
